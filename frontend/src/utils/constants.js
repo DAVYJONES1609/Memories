ede6146a35f7daa63c196b103d72fe1d8daa16b3
VipulVibhasSingh <98639789+VipulVibhasSingh@users.noreply.github.com>
2023-03-30 01:50:12 +0530

    Add files via upload

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,37 @@
+import HomeIcon from '@mui/icons-material/Home';
+import React from 'react'
+import ToysIcon from '@mui/icons-material/Toys';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import ShieldIcon from '@mui/icons-material/Shield';
+import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
+import SportsIcon from '@mui/icons-material/Sports';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import SportsKabaddiIcon from '@mui/icons-material/SportsKabaddi';
+import SportsHandballIcon from '@mui/icons-material/SportsHandball';
+
+
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'Home', icon: <HomeIcon />, },
+  { name: 'Trending', icon: <OndemandVideoIcon />, },
+  { name: 'Valorant', icon: <SportsEsportsIcon />, },
+  { name: 'GTA', icon: <ToysIcon />, },
+  { name: 'Witcher', icon: <ArrowOutwardIcon />, },
+  { name: 'God Of War', icon: <ShieldIcon />, },
+  { name: 'Call Of Duty', icon: <SportsIcon />, },
+  { name: 'BGMI', icon: <SportsIcon /> },
+  { name: 'Pokemon', icon: <SportsIcon />, },
+  { name: 'Sekiro', icon: <SportsKabaddiIcon />, },
+  { name: 'Elden Rings', icon: <SportsHandballIcon />, },
+  { name: 'Skyrim', icon: <SportsIcon />, }
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
